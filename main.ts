36c74c8ff13e37f440603e2bfec57a9bb3d64b8e
zhouqing801023 <68157926+zhouqing801023@users.noreply.github.com>
2022-07-21 14:11:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,19 +4,19 @@ basic.forever(function () {
     bearing = input.compassHeading()
     if (bearing < 45 || bearing > 315) {
         basic.showString("N")
-    } else if (bearing < 90 || bearing > 0) {
+    } else if (bearing < 90 && bearing > 0) {
         basic.showString("NE")
-    } else if (bearing < 135 || bearing > 45) {
+    } else if (bearing < 135 && bearing > 45) {
         basic.showString("E")
-    } else if (bearing < 180 || bearing > 90) {
+    } else if (bearing < 180 && bearing > 90) {
         basic.showString("SE")
-    } else if (bearing < 225 || bearing > 135) {
+    } else if (bearing < 225 && bearing > 135) {
         basic.showString("S")
-    } else if (bearing < 270 || bearing > 180) {
+    } else if (bearing < 270 && bearing > 180) {
         basic.showString("SW")
-    } else if (bearing < 315 || bearing > 225) {
+    } else if (bearing < 315 && bearing > 225) {
         basic.showString("W")
-    } else if (bearing < 360 || bearing > 270) {
+    } else if (bearing < 360 && bearing > 270) {
         basic.showString("NW")
     } else {
         basic.showString("+")
